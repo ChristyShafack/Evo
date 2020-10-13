1bbdcb9b03833d70e12f908ac8d110746fff525b
christy <christyshafack@gmail.com>
2020-10-13 11:42:16 +0100

    Html page

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -3,24 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-list',
-  template: `
-    <div>
-        <h1>Upcoming Angular Events</h1>
-        <hr>
-        <div class="well hoverwell thumbnail">
-        <h2>{{ event.name }}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
-        <div>
-            <span>Location: Address: {{event.location.address}}</span>
-            <span>&nbsp;</span>
-            <span>Location: City: {{event.location.city}}</span>
-            <span>Location: Country: {{event.location.country}}</span>
-        </div>
-        </div>
-    </div>
-    ` 
+  templateUrl: './events-list.component.html'
 })
 export class EventsListComponent {
   event = {
