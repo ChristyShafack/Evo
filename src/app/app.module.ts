d3304951d09ed077f5aa873a31cddc113fb38d5b
christy <christyshafack@gmail.com>
2020-10-15 23:56:27 +0100

    Created NavBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventsThumbnailComponent } from './events/events-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
 import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
+    NavBarComponent,
     EventsAppComponent,
     EventsListComponent,
     EventsThumbnailComponent,
