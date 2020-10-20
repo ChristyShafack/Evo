27183a8487c5ef18b78018c8b6dcd9674d24580e
christy <christyshafack@gmail.com>
2020-10-20 22:48:40 +0100

    User login form

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common'
+import { LoginComponent } from './login.component'
 import { NgModule } from '@angular/core'
 import { ProfileComponent } from './profile.component'
 import { RouterModule } from '@angular/router'
@@ -10,7 +11,8 @@ import { userRoutes } from './user.routes'
     RouterModule.forChild(userRoutes)
   ],
   declarations: [
-    ProfileComponent
+    ProfileComponent,
+    LoginComponent
   ],
   providers: [
 
