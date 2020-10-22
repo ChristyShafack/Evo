e1c549832afd3f9d3a8738e80d448b623131929c
christy <christyshafack@gmail.com>
2020-10-22 15:01:17 +0100

    Populating fields and saving user data in reactive forms

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -22,8 +22,12 @@ export class ProfileComponent implements OnInit {
       lastName: lastName  
     })
   }
-
+  saveProfile(formValues){
+    this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
+    this.router.navigate(['events'])
+  }
   cancel(){
     this.router.navigate(['events'])
   }
+ 
 }
